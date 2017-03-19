39b2d0857d0a6527a0764649cb6dbe692f56aa5f
Bragdish <rbragdish@gmail.com>
2017-03-19 08:04:06 -0400

    Created LibraryAssignment file

---
+++ src/mongoapplication/LibraryAssignment.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mongoapplication;
+
+/**
+ *
+ * @author Makubex
+ */
+public class LibraryAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FirstPage page1=new FirstPage();
+        page1.setVisible(true);
+    }
+    
+}
